1d6fd1b417867efae7c5b8867b75b529b948d0e5
Andrew Maguire <andrewm4894@gmail.com>
2020-05-25 20:42:55 +0100

    dev

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_chart_data_urls():
-    url = "https://127.0.0.1:19999/api/v1/charts"
+    url = "http://127.0.0.1:19999/api/v1/charts"
     r = requests.get(url)
     charts = r.json().get('charts')
     chart_data_urls = [charts[chart].get('data_url') for chart in charts]
