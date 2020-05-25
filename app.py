1e4bcce173443e90220d9a6eb044d8debae981cd
Andrew Maguire <andrewm4894@gmail.com>
2020-05-25 16:59:45 +0100

    dev

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -10,9 +12,16 @@ def hello_world():
 
 @app.route('/ks', methods=['GET'])
 def get_data():
-    context_chart = request.args.get('context_chart', 'xxx')
+    now = datetime.now()
+    context_chart = request.args.get('context_chart', 'system.cpu')
+    window_start = request.args.get('context_chart', now)
+    window_end = request.args.get('context_chart', now - timedelta(minutes=1))
     response = {
-        "context_chart": context_chart,
+        "info": dict(
+            context_chart=context_chart,
+            window_start=window_start,
+            window_end=window_end
+        ),
         "results": {
             1: {"chart_name": "blah", "score": 0.33, "p": 0.01, "rank": 1},
             2: {"chart_name": "foo", "score": 0.2, "p": 0.05, "rank": 2}
