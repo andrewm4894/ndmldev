d4afd571ed0b97798d34dbcff89f9da313e9386c
Andrew Maguire <andrewm4894@gmail.com>
2020-05-25 16:55:15 +0100

    dev

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,9 @@ def hello_world():
 
 @app.route('/ks', methods=['GET'])
 def get_data():
+    context_chart = request.args.get('context_chart', 'xxx')
     response = {
+        "context_chart": context_chart,
         "results": {
             1: {"chart_name": "blah", "score": 0.33, "p": 0.01, "rank": 1},
             2: {"chart_name": "foo", "score": 0.2, "p": 0.05, "rank": 2}
