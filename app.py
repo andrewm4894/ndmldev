0a3f881d39217b5ceb351119f185911ca542a274
Andrew Maguire <andrewm4894@gmail.com>
2020-05-25 16:51:14 +0100

    dev

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello_world():
 @app.route('/ks', methods=['GET'])
 def get_data():
     response = {
-        "request_args": request.args.to_dict(),
+        "context_chart": request.args.get('context_chart'),
         "results": {
             1: {"chart_name": "blah", "score": 0.33, "p": 0.01, "rank": 1},
             2: {"chart_name": "foo", "score": 0.2, "p": 0.05, "rank": 2}
