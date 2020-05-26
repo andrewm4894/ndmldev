202b4aea662499b1b7fed6ad43d01c7b7b1bc3e6
Andrew Maguire <andrewm4894@gmail.com>
2020-05-26 11:34:17 +0100

    dev

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from utils import get_chart_data_urls, get_chart_df, do_ks, get_chart_list
 app = Flask(__name__)
 
 
-@app.route('/tmp')
-def tmp():
+@app.route('/ks')
+def ks():
     before = int(datetime.now().timestamp())
     after = before - 500
     window_end = before - 10
@@ -44,28 +44,28 @@ def tmp():
 
 
 @app.route('/')
-def home_info():
+def home():
     response = dict(
         ks='/ks'
     )
     return response
 
 
-@app.route('/ks', methods=['GET'])
+@app.route('/tmp', methods=['GET'])
 def xdo_ks():
     now = datetime.now()
-    context_chart = request.args.get('context_chart', 'system.cpu')
-    window_start = request.args.get('window_start', now)
-    window_end = request.args.get('window_end', now - timedelta(minutes=1))
+    before = request.args.get('before', now)
+    after = request.args.get('after', now-100)
+    highlight_before = request.args.get('highlight_before', now)
+    highlight_after = request.args.get('highlight_after', now-100)
     response = {
         "info": dict(
-            context_chart=context_chart,
-            window_start=window_start,
-            window_end=window_end
+            before=before,
+            after=after,
+            highlight_before=highlight_before,
+            highlight_after=highlight_after
         ),
         "results": {
-            1: {"chart_name": "blah", "score": 0.33, "p": 0.01, "rank": 1},
-            2: {"chart_name": "foo", "score": 0.2, "p": 0.05, "rank": 2}
         }
     }
     return response
