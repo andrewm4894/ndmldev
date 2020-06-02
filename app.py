fd9c256e53b967d5052303b4a378bc8f09abe85f
Andrew Maguire <andrewm4894@gmail.com>
2020-06-02 15:58:52 +0100

    dev

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def results():
         charts = [
             {
                 "id": result,
-                "title": f"{results[result]['rank']} - {result} (score={results[result]['score']})",
+                "title": f"{results[result]['rank']} - {result} (ks={results[result]['summary']['ks_mean']}, p={results[result]['summary']['p_mean']})",
                 "after": baseline_after,
                 "before": highlight_before
             } for result in results
