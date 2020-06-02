93f4bc1ea149010f17302ac47014ee21769f5e10
Andrew Maguire <andrewm4894@gmail.com>
2020-06-02 13:38:46 +0100

    dev

--- app.py
+++ app.py
@@ -69,13 +69,12 @@ def results():
     netdata_url = request.args.get('url')
     netdata_host = urlparse(netdata_url).netloc
     netdata_params = parse_qs(netdata_url)
-    print(netdata_params)
     after = int(netdata_params.get('after')[0])
     before = int(netdata_params.get('before')[0])
     highlight_after = int(netdata_params.get('highlight_after')[0])
     highlight_before = int(netdata_params.get('highlight_before')[0])
-    after_secs = str(now_ts - (after / 1000))
-    before_secs = str(now_ts - (before / 1000))
+    after_secs = str(now_ts - (after))
+    before_secs = str(now_ts - (before))
     charts = [
         {"id": "system.cpu", "title": "cpu", "after": after_secs, "before": before_secs},
         {"id": "system.load", "title": "load", "after": after_secs, "before": before_secs},
