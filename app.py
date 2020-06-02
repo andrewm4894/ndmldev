21a2f192dedf1bd6b57ad3ce76a27a7f1f144b21
Andrew Maguire <andrewm4894@gmail.com>
2020-06-02 14:02:06 +0100

    dev

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ def results():
     results = {}
     for chart in get_chart_list(starts_with=starts_with):
         df = get_chart_df(chart, baseline_after, highlight_before)
+        app.logger.info(df)
         if df:
             if len(df) > 0:
                 ks_results = do_ks(df, baseline_after, baseline_before, highlight_after, highlight_before)
