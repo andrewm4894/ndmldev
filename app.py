d92e3c7ef2534306982ee3e99e6b084f35d03515
Andrew Maguire <andrewm4894@gmail.com>
2020-06-02 15:17:07 +0100

    dev

--- app.py
+++ app.py
@@ -11,53 +11,6 @@ from ks import do_ks
 
 
 app = Flask(__name__)
-app.logger.setLevel(logging.INFO)
-
-
-@app.route('/ks')
-def ks():
-    params = parse_params(request)
-    highlight_before = params['highlight_before']
-    highlight_after = params['highlight_after']
-    baseline_before = params['baseline_before']
-    baseline_after = params['baseline_after']
-    rank_by = params['rank_by']
-    starts_with = params['starts_with']
-    response_format = params['format']
-    results = {}
-    for chart in get_chart_list(starts_with=starts_with):
-        df = get_chart_df(chart, baseline_after, highlight_before)
-        if len(df) > 0:
-            ks_results = do_ks(df, baseline_after, baseline_before, highlight_after, highlight_before)
-            if ks_results:
-                results[chart] = ks_results
-    df_rank = pd.DataFrame(data=[[c, results[c]['summary'][rank_by]] for c in results], columns=['chart', 'score'])
-    df_rank['rank'] = df_rank['score'].rank(method='first')
-    for _, row in df_rank.iterrows():
-        results[row['chart']]['rank'] = int(row['rank'])
-        results[row['chart']]['score'] = float(row['score'])
-    results = OrderedDict(sorted(results.items(), key=lambda t: t[1]["rank"]))
-    print(results)
-    if response_format == 'json':
-        return json.dumps([results])
-    elif response_format == 'html':
-        max_rank = 50
-        render_info = []
-        for chart in results:
-            rank = results[chart]['rank']
-            if rank >= max_rank:
-                break
-            else:
-                score = results[chart]['score']
-                render_info.append({
-                    "chart_id": chart,
-                    "title": f"{rank} - {chart} - {score}",
-                    "before": baseline_after,
-                    "after": highlight_before,
-                })
-        return render_template('results.html', render_info=render_info)
-    else:
-        return None
 
 
 @app.route('/')
@@ -86,6 +39,10 @@ def results():
             ks_results = do_ks(df, baseline_after, baseline_before, highlight_after, highlight_before)
             if ks_results:
                 results[chart] = ks_results
+    print(results)
+    XXX
+
+
     df_rank = pd.DataFrame(data=[[c, results[c]['summary'][rank_by]] for c in results], columns=['chart', 'score'])
     df_rank['rank'] = df_rank['score'].rank(method='first')
     for _, row in df_rank.iterrows():
--- ks.py
+++ ks.py
@@ -4,26 +4,35 @@ import numpy as np
 from utils import filter_useless_cols
 
 
-def do_ks(df, baseline_start, baseline_end, window_start, window_end):
+def do_ks(df, baseline_start, baseline_end, highlight_start, highlight_end):
+
     df = df._get_numeric_data()
     df = filter_useless_cols(df)
+
     if len(df.columns) > 0:
+
         df_baseline = df[(df.index >= baseline_start) & (df.index <= baseline_end)]
-        df_window = df[(df.index >= window_start) & (df.index <= window_end)]
+        df_highlight = df[(df.index >= highlight_start) & (df.index <= highlight_end)]
         results = {
             'summary': {},
             'detail': {}
         }
+
         for col in df_baseline.columns:
-            res = ks_2samp(df_baseline[col], df_window[col])
+
+            res = ks_2samp(df_baseline[col], df_highlight[col])
             results['detail'][col] = {"ks": float(round(res[0], 4)), "p": float(round(res[1], 4))}
+
         results['summary']['ks_mean'] = float(round(np.mean([results['detail'][res]['ks'] for res in results['detail']]), 4))
         results['summary']['ks_min'] = float(round(np.min([results['detail'][res]['ks'] for res in results['detail']]), 4))
         results['summary']['ks_max'] = float(round(np.max([results['detail'][res]['ks'] for res in results['detail']]), 4))
         results['summary']['p_mean'] = float(round(np.mean([results['detail'][res]['p'] for res in results['detail']]), 4))
         results['summary']['p_min'] = float(round(np.min([results['detail'][res]['p'] for res in results['detail']]), 4))
         results['summary']['p_max'] = float(round(np.max([results['detail'][res]['p'] for res in results['detail']]), 4))
+
         return results
+
     else:
+
         return None
 
