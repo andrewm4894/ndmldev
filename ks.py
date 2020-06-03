a82e57d0756ce5a55a05e1bacd16b32f7b28d46d
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 16:22:33 +0100

    dev

--- ks.py
+++ ks.py
@@ -4,33 +4,28 @@ from scipy.stats import ks_2samp
 import numpy as np
 import pandas as pd
 
-from utils import filter_useless_cols
-
 
 def do_ks(df, baseline_start, baseline_end, highlight_start, highlight_end):
-    if len(df.columns) > 0:
-        df_baseline = df[(df.index >= baseline_start) & (df.index <= baseline_end)]
-        df_highlight = df[(df.index >= highlight_start) & (df.index <= highlight_end)]
-        results = {
-            'summary': {},
-            'detail': {}
-        }
-        for col in df_baseline.columns:
-            data_baseline = df_baseline[col].dropna().values
-            data_highlight = df_highlight[col].dropna().values
-            if len(data_baseline) > 0 and len(data_highlight) > 0:
-                res = ks_2samp(data_baseline, data_highlight)
-                results['detail'][col] = {"ks": float(round(res[0], 4)), "p": float(round(res[1], 4))}
-        if len(results['detail']) > 0:
-            results['summary']['ks_mean'] = float(round(np.mean([results['detail'][res]['ks'] for res in results['detail']]), 4))
-            results['summary']['ks_min'] = float(round(np.min([results['detail'][res]['ks'] for res in results['detail']]), 4))
-            results['summary']['ks_max'] = float(round(np.max([results['detail'][res]['ks'] for res in results['detail']]), 4))
-            results['summary']['p_mean'] = float(round(np.mean([results['detail'][res]['p'] for res in results['detail']]), 4))
-            results['summary']['p_min'] = float(round(np.min([results['detail'][res]['p'] for res in results['detail']]), 4))
-            results['summary']['p_max'] = float(round(np.max([results['detail'][res]['p'] for res in results['detail']]), 4))
-            return results
-        else:
-            return None
+    df_baseline = df[(df.index >= baseline_start) & (df.index <= baseline_end)]
+    df_highlight = df[(df.index >= highlight_start) & (df.index <= highlight_end)]
+    results = {
+        'summary': {},
+        'detail': {}
+    }
+    for col in df_baseline.columns:
+        data_baseline = df_baseline[col].dropna().values
+        data_highlight = df_highlight[col].dropna().values
+        if len(data_baseline) > 0 and len(data_highlight) > 0:
+            res = ks_2samp(data_baseline, data_highlight)
+            results['detail'][col] = {"ks": float(round(res[0], 4)), "p": float(round(res[1], 4))}
+    if len(results['detail']) > 0:
+        results['summary']['ks_mean'] = float(round(np.mean([results['detail'][res]['ks'] for res in results['detail']]), 4))
+        results['summary']['ks_min'] = float(round(np.min([results['detail'][res]['ks'] for res in results['detail']]), 4))
+        results['summary']['ks_max'] = float(round(np.max([results['detail'][res]['ks'] for res in results['detail']]), 4))
+        results['summary']['p_mean'] = float(round(np.mean([results['detail'][res]['p'] for res in results['detail']]), 4))
+        results['summary']['p_min'] = float(round(np.min([results['detail'][res]['p'] for res in results['detail']]), 4))
+        results['summary']['p_max'] = float(round(np.max([results['detail'][res]['p'] for res in results['detail']]), 4))
+        return results
     else:
         return None
 
