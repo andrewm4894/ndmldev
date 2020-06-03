3a173665a91380c6787b0ae45f08ba453659a95e
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 13:27:35 +0100

    dev

--- get_data.py
+++ get_data.py
@@ -7,15 +7,16 @@ import trio
 
 def get_chart_df(chart, after, before, host: str = '127.0.0.1:19999', format: str = 'json', numeric_only: bool = True):
     url = f"http://{host}/api/v1/data?chart={chart}&after={after}&before={before}&format={format}"
-    print(url)
     r = requests.get(url)
     r_json = r.json()
-    print(r_json)
     df = pd.DataFrame(r_json['data'], columns=['time_idx'] + r_json['labels'][1:])
-    if numeric_only:
-        df = df._get_numeric_data()
-    df = df.set_index('time_idx')
-    return df
+    if len(df) == 0:
+        return df
+    else:
+        if numeric_only:
+            df = df._get_numeric_data()
+        df = df.set_index('time_idx')
+        return df
 
 
 async def get_chart_df_async(api_call, data):
