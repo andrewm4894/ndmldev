03ec8c1b761a9d107dbbd46e498a09639ad182b3
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 15:44:14 +0100

    dev

--- get_data.py
+++ get_data.py
@@ -10,11 +10,11 @@ from utils import get_chart_list
 
 def get_chart_df(chart, after, before, host: str = '127.0.0.1:19999', format: str = 'json', numeric_only: bool = True):
     url = f"http://{host}/api/v1/data?chart={chart}&after={after}&before={before}&format={format}"
-    print(url)
+    #print(url)
     r = requests.get(url)
     r_json = r.json()
     df = pd.DataFrame(r_json['data'], columns=['time_idx'] + r_json['labels'][1:])
-    print(df.shape)
+    #print(df.shape)
     if len(df) == 0:
         return df
     else:
@@ -26,12 +26,12 @@ def get_chart_df(chart, after, before, host: str = '127.0.0.1:19999', format: st
 
 async def get_chart_df_async(api_call, data):
     url, chart = api_call
-    print(url)
+    #print(url)
     r = await asks.get(url)
     r_json = r.json()
     df = pd.DataFrame(r_json['data'], columns=['time_idx'] + r_json['labels'][1:])
     df = df.set_index('time_idx').add_prefix(f'{chart}__')
-    print(df.shape)
+    #print(df.shape)
     data[chart] = df
 
 
