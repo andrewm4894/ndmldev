4a7e85f1e25f86154f1ff537b81fecf5910b4ac9
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 16:53:31 +0100

    dev

--- ks_cli.py
+++ ks_cli.py
@@ -86,8 +86,8 @@ if run_mode == 'async':
         #df_baseline = df[(df.index >= baseline_after) & (df.index <= baseline_before)]
         #df_highlight = df[(df.index >= highlight_after) & (df.index <= highlight_before)]
 
-        df_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}')
-        df_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}')
+        df_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}').copy()
+        df_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}').copy()
 
         #df_baseline = df.iloc[baseline_after:baseline_before]
         #print(df_baseline.shape)
