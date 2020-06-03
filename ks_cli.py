94607ff44dc02e0d07eb92a655b24e11e3ab1384
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 13:09:59 +0100

    dev

--- ks_cli.py
+++ ks_cli.py
@@ -1,6 +1,10 @@
 import argparse
 from urllib.parse import parse_qs, urlparse
 
+import trio
+
+from get_data import get_charts_df_async
+from ks import do_ks, rank_results
 from utils import get_chart_list
 
 parser = argparse.ArgumentParser()
@@ -25,11 +29,20 @@ starts_with = None
 window_size = highlight_before - highlight_after
 baseline_before = highlight_after - 1
 baseline_after = baseline_before - (window_size * baseline_window_multiplier)
+rank_by = 'ks_max'
 
 charts = get_chart_list(starts_with=starts_with, host=host)
 api_calls = [
     (f'http://{host}/api/v1/data?chart={chart}&after={baseline_after}&before={highlight_before}&format=json', chart)
     for chart in charts
 ]
-
-print(api_calls)
+df = trio.run(get_charts_df_async, api_calls)
+results = {}
+for chart in charts:
+    chart_cols = [col for col in df.columns if col.startswith(f'{chart}__')]
+    df_chart = df[chart_cols]
+    ks_results = do_ks(df_chart, baseline_after, baseline_before, highlight_after, highlight_before)
+    if ks_results:
+        results[chart] = ks_results
+results = rank_results(results, rank_by, ascending=False)
+print(results)
