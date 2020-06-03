1d5c4b3b6f76aab07fb95257619879007111fa98
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 22:11:17 +0100

    dev

--- scratchpad.py
+++ scratchpad.py
@@ -1,21 +1,23 @@
 #%%
 
-from get_data import get_chart_df, get_chart_arr
+import requests
+import numpy as np
+
+chart = 'system.cpu'
+host='34.75.216.243:19999'
+after=1591184028
+before=1591184889
+
+url = f"http://{host}/api/v1/data?chart={chart}&after={after}&before={before}&format={format}"
+r = requests.get(url)
+r_json = r.json()
+print(r_json['data'])
+data = np.array(r_json['data'], dtype=[(col, '<f4') for col in r_json['labels']])
 
 #%%
 
+[(col, '<f4') for col in r_json['labels']]
 
 #%%
-
-#df = get_chart_df('system.cpu', host='34.75.216.243:19999', after=1591184028, before=1591184889)
-names, arr = get_chart_arr('system.cpu', host='34.75.216.243:19999', after=1591184028, before=1591184889)
-
-#%%
-
-arr
-
-#%%
-
-names
 
 #%%
