bd2027263a9d1d170ecf86f2af270ad540fe8b2c
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 16:36:08 +0100

    dev

--- ks_cli.py
+++ ks_cli.py
@@ -90,8 +90,8 @@ if run_mode == 'async':
         for col in df.columns:
             results.append(
                 ks_2samp(
-                    df[col].sample(0.5),
-                    df[col].sample(0.5)
+                    df[col].sample(frac=0.5),
+                    df[col].sample(frac=0.5)
                 )
             )
 
