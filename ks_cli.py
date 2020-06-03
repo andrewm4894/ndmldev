be2c623db0e0c0b7569fb40f72828ffdcf4d10ef
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 22:25:03 +0100

    dev

--- ks_cli.py
+++ ks_cli.py
@@ -69,8 +69,8 @@ if run_mode == 'async':
     df = trio.run(get_charts_df_async, api_calls)
     df = df._get_numeric_data()
     df = filter_useless_cols(df)
-    data_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}').transpose().values
-    data_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}').transpose().values
+    data_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}').values
+    data_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}').values
     time_got_data = time.time()
     print(f'... time start to data = {time_got_data - time_start}')
 
