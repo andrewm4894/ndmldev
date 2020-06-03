99c0d0dce4366676c4ce0af4afd9fa2d37039ebc
Andrew Maguire <andrewm4894@gmail.com>
2020-06-03 13:07:57 +0100

    dev

--- ks_cli.py
+++ ks_cli.py
@@ -1,5 +1,7 @@
 import argparse
-from urllib.parse import parse_qs
+from urllib.parse import parse_qs, urlparse
+
+from utils import get_chart_list
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -10,10 +12,24 @@ args = parser.parse_args()
 # parse args
 url = args.url
 
-url_params = parse_qs(url)
-after = url_params.get('after')
-before = url_params.get('before')
-highlight_after = url_params.get('highlight_after')
-highlight_before = url_params.get('highlight_before')
+baseline_window_multiplier = 2
 
-print(url)
+url_params = parse_qs(url)
+url_parse = urlparse(url)
+host = url_parse.netloc
+after = int(int(url_params.get('after')[0]) / 1000)
+before = int(int(url_params.get('before')[0]) / 1000)
+highlight_after = int(int(url_params.get('highlight_after')[0]) / 1000)
+highlight_before = int(int(url_params.get('highlight_before')[0]) / 1000)
+starts_with = None
+window_size = highlight_before - highlight_after
+baseline_before = highlight_after - 1
+baseline_after = baseline_before - (window_size * baseline_window_multiplier)
+
+charts = get_chart_list(starts_with=starts_with, host=host)
+api_calls = [
+    (f'http://{host}/api/v1/data?chart={chart}&after={baseline_after}&before={highlight_before}&format=json', chart)
+    for chart in charts
+]
+
+print(api_calls)
