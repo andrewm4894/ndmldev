45349f03f753c9bde06de12e26995426c3e469a4
Andrew Maguire <andrewm4894@gmail.com>
2020-06-04 21:42:42 +0100

    dev

--- ks_cli.py
+++ ks_cli.py
@@ -75,16 +75,18 @@ for n in range(arr_baseline.shape[1]):
     ks_stat, p_value = ks_2samp(arr_baseline[:, n], arr_highlight[:, n], mode='asymp')
     results.append([ks_stat, p_value])
 time_got_ks = time.time()
-print(f'... time data to ks = {time_got_ks - time_got_data}')
+print(f'... time data to ks = {round(time_got_ks - time_got_data,2)}')
 
 # wrangle results
 results = zip([[col.split('__')[0], col.split('__')[1]] for col in list(df.columns)], results)
 results = [[x[0][0], x[0][1], x[1][0], x[1][1]] for x in results]
 
+# df_results
 df_results = pd.DataFrame(results, columns=['chart', 'dimension', 'ks', 'p'])
 df_results['rank'] = df_results[rank_by_var].rank(method='first', ascending=rank_asc)
 df_results = df_results.sort_values('rank')
 
+# df_results_chart
 df_results_chart = df_results.groupby(['chart'])[['ks', 'p']].agg(['mean', 'min', 'max'])
 df_results_chart.columns = ['_'.join(col) for col in df_results_chart.columns]
 df_results_chart = df_results_chart.reset_index()
@@ -92,10 +94,10 @@ df_results_chart['rank'] = df_results_chart[rank_by].rank(method='first', ascend
 df_results_chart = df_results_chart.sort_values('rank')
 
 time_got_results = time.time()
-print(f'... time ks to results = {time_got_results - time_got_ks}')
+print(f'... time ks to results = {round(time_got_results - time_got_ks,2)}')
 
 time_done = time.time()
-print(f'... time total = {time_done - time_start}')
+print(f'... time total = {round(time_done - time_start,2)}')
 
 print(df_results)
 print(df_results_chart)
