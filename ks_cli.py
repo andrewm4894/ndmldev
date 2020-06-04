0ec3516de4d560b4faa8099662a3b7341bdbcc91
Andrew Maguire <andrewm4894@gmail.com>
2020-06-04 16:23:24 +0100

    dev

--- ks_cli.py
+++ ks_cli.py
@@ -1,17 +1,12 @@
 import argparse
-import multiprocessing
 import time
-from multiprocessing import Pool
-from threading import Thread
 from urllib.parse import parse_qs, urlparse
 
 import trio
 import pandas as pd
-from scipy import stats
 from scipy.stats import ks_2samp
 
-from get_data import get_charts_df_async, get_chart_df
-from ks import do_ks, rank_results
+from get_data import get_charts_df_async
 from utils import get_chart_list, filter_useless_cols
 
 time_start = time.time()
@@ -23,22 +18,13 @@ parser.add_argument(
 parser.add_argument(
     '--remote', type=str, nargs='?', default='no'
 )
-parser.add_argument(
-    '--run_mode', type=str, nargs='?', default='default'
-)
-parser.add_argument(
-    '--ks_mode', type=str, nargs='?', default='default'
-)
 args = parser.parse_args()
 
 # parse args
 url = args.url
 remote = args.remote
-run_mode = args.run_mode
-ks_mode = args.ks_mode
 
 baseline_window_multiplier = 2
-
 url_params = parse_qs(url)
 url_parse = urlparse(url)
 
@@ -60,90 +46,36 @@ rank_by = 'ks_max'
 results = {}
 charts = get_chart_list(starts_with=starts_with, host=host)
 
-if run_mode == 'async':
+# get data
+api_calls = [
+    (f'http://{host}/api/v1/data?chart={chart}&after={baseline_after}&before={highlight_before}&format=json', chart)
+    for chart in charts
+]
+df = trio.run(get_charts_df_async, api_calls)
+df = df._get_numeric_data()
+df = filter_useless_cols(df)
+arr_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}').values
+arr_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}').values
+time_got_data = time.time()
+print(f'... time start to data = {time_got_data - time_start}')
 
-    api_calls = [
-        (f'http://{host}/api/v1/data?chart={chart}&after={baseline_after}&before={highlight_before}&format=json', chart)
-        for chart in charts
-    ]
-    df = trio.run(get_charts_df_async, api_calls)
-    df = df._get_numeric_data()
-    df = filter_useless_cols(df)
-    arr_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}').values
-    arr_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}').values
-    time_got_data = time.time()
-    print(f'... time start to data = {time_got_data - time_start}')
+# get ks
+results = []
+for n in range(arr_baseline.shape[1]):
+    ks_stat, p_value = ks_2samp(arr_baseline[:, n], arr_highlight[:, n], mode='asymp')
+    results.append([ks_stat, p_value])
+time_got_ks = time.time()
+print(f'... time data to ks = {time_got_ks - time_got_data}')
 
-    results = []
-    for n in range(arr_baseline.shape[1]):
-        ks_stat, p_value = ks_2samp(arr_baseline[:, n], arr_highlight[:, n], mode='asymp')
-        results.append([ks_stat, p_value])
-
-    time_got_ks = time.time()
-    print(f'... time data to ks = {time_got_ks - time_got_data}')
-
-    # wrangle results
-    results = zip([[col.split('__')[0], col.split('__')[1]] for col in list(df.columns)], results)
-    results = [[x[0][0], x[0][1], x[1][0], x[1][1]] for x in results]
-    df_results = pd.DataFrame(results, columns=['chart', 'dimension', 'ks', 'p'])
-    print(df_results)
-
-    XXX
-
-elif run_mode == 'default':
-
-    for chart in charts:
-        df = get_chart_df(chart, after=baseline_after, before=highlight_before, host=host)
-        df = filter_useless_cols(df)
-        if len(df) > 0:
-            ks_results = do_ks(df, baseline_after, baseline_before, highlight_after, highlight_before)
-            if ks_results:
-                results[chart] = ks_results
-
-elif run_mode == 'multi':
-
-    def do_multi(params):
-        chart, baseline_after, baseline_before, highlight_after, highlight_before = params
-        df = get_chart_df(chart, after=baseline_after, before=highlight_before, host=host)
-        if len(df) > 0:
-            ks_results = do_ks(df, baseline_after, baseline_before, highlight_after, highlight_before)
-            if ks_results:
-                return {chart: ks_results}
-            else:
-                return None
-        else:
-            return None
-
-    p = Pool(processes=(multiprocessing.cpu_count()-1))
-    params_list = [(chart, baseline_after, baseline_before, highlight_after, highlight_before) for chart in charts]
-    results = p.map(do_multi, params_list)
-    results = [result for result in results if result]
-    results = {list(d)[0]: d[list(d)[0]] for d in results}
-
-elif run_mode == 'thread':
-
-    results = {}
-
-    def do_thread(params):
-        chart, baseline_after, baseline_before, highlight_after, highlight_before = params
-        df = get_chart_df(chart, after=baseline_after, before=highlight_before, host=host)
-        if len(df) > 0:
-            ks_results = do_ks(df, baseline_after, baseline_before, highlight_after, highlight_before)
-            if ks_results:
-                results[chart] = ks_results
-
-    threads = []
-    params_list = [(chart, baseline_after, baseline_before, highlight_after, highlight_before) for chart in charts]
-    for param in params_list:
-        process = Thread(target=do_thread, args=[param])
-        process.start()
-        threads.append(process)
-
-    for process in threads:
-        process.join()
-
-results = rank_results(results, rank_by, ascending=False)
-#print(results)
+# wrangle results
+results = zip([[col.split('__')[0], col.split('__')[1]] for col in list(df.columns)], results)
+results = [[x[0][0], x[0][1], x[1][0], x[1][1]] for x in results]
+df_results = pd.DataFrame(results, columns=['chart', 'dimension', 'ks', 'p'])
+time_got_results = time.time()
+print(f'... time ks to results = {time_got_results - time_got_ks}')
 
 time_done = time.time()
 print(f'... time total = {time_done - time_start}')
+
+print(df_results)
+
