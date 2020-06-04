32d3cf584b797f30a57bd0fc6c8c972e27da09a6
Andrew Maguire <andrewm4894@gmail.com>
2020-06-04 21:49:27 +0100

    dev

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import trio
 import asks
 from flask import Flask, request, render_template, jsonify
+from scipy.stats import ks_2samp
 
 from get_data import get_charts_df_async, get_chart_df
 from utils import get_chart_list, parse_params
@@ -17,16 +18,6 @@ app.config["JSON_SORT_KEYS"] = False
 logging.basicConfig(level=logging.INFO)
 
 
-@app.route("/tmp")
-def tmp():
-    api_calls = [
-        ("http://london.my-netdata.io/api/v1/data?chart=system.cpu&format=json", "system.cpu"),
-        ("http://london.my-netdata.io/api/v1/data?chart=system.load&format=json", "system.load")
-    ]
-    df = trio.run(get_charts_df_async, api_calls)
-    return 'hello'
-
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -35,7 +26,7 @@ def home():
 @app.route('/results')
 def results():
 
-    start_time = time.time()
+    time_start = time.time()
 
     # get params
     params = parse_params(request)
@@ -49,34 +40,52 @@ def results():
     remote_host = params['remote_host']
     local_host = params['local_host']
     run_mode = params['run_mode']
+    rank_by_var = rank_by.split('_')[0]
+    rank_by_agg = rank_by.split('_')[1]
 
-    results = {}
+    # get charts to pull data for
+    charts = get_chart_list(starts_with=starts_with, host=remote_host)
+    api_calls = [
+        (f'http://{remote_host}/api/v1/data?chart={chart}&after={baseline_after}&before={highlight_before}&format=json', chart)
+        for chart in charts
+    ]
+    df = trio.run(get_charts_df_async, api_calls)
+    df = df._get_numeric_data()
+    df = filter_useless_cols(df)
+    arr_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}').values
+    arr_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}').values
+    time_got_data = time.time()
+    print(f'... time start to data = {time_got_data - time_start}')
 
-    if run_mode == 'async':
-        # get charts to pull data for
-        charts = get_chart_list(starts_with=starts_with, host=remote_host)
-        api_calls = [
-            (f'http://{remote_host}/api/v1/data?chart={chart}&after={baseline_after}&before={highlight_before}&format=json', chart)
-            for chart in charts
-        ]
-        df = trio.run(get_charts_df_async, api_calls)
-        for chart in charts:
-            chart_cols = [col for col in df.columns if col.startswith(f'{chart}__')]
-            df_chart = df[chart_cols]
-            ks_results = do_ks(df_chart, baseline_after, baseline_before, highlight_after, highlight_before)
-            if ks_results:
-                results[chart] = ks_results
-    else:
-        for chart in get_chart_list(starts_with=starts_with, host=remote_host):
-            df = get_chart_df(chart, after=baseline_after, before=highlight_before, host=remote_host)
-            if len(df) > 0:
-                ks_results = do_ks(df, baseline_after, baseline_before, highlight_after, highlight_before)
-                if ks_results:
-                    results[chart] = ks_results
+    # get ks
+    results = []
+    for n in range(arr_baseline.shape[1]):
+        ks_stat, p_value = ks_2samp(arr_baseline[:, n], arr_highlight[:, n], mode='asymp')
+        results.append([ks_stat, p_value])
+    time_got_ks = time.time()
+    print(f'... time data to ks = {round(time_got_ks - time_got_data, 2)}')
 
-    results = rank_results(results, rank_by, ascending=False)
+    # wrangle results
+    results = zip([[col.split('__')[0], col.split('__')[1]] for col in list(df.columns)], results)
+    results = [[x[0][0], x[0][1], x[1][0], x[1][1]] for x in results]
 
-    app.logger.info(f"time taken = {time.time()-start_time}")
+    # df_results
+    df_results = pd.DataFrame(results, columns=['chart', 'dimension', 'ks', 'p'])
+    df_results['rank'] = df_results[rank_by_var].rank(method='first', ascending=rank_asc)
+    df_results = df_results.sort_values('rank')
+
+    # df_results_chart
+    df_results_chart = df_results.groupby(['chart'])[['ks', 'p']].agg(['mean', 'min', 'max'])
+    df_results_chart.columns = ['_'.join(col) for col in df_results_chart.columns]
+    df_results_chart = df_results_chart.reset_index()
+    df_results_chart['rank'] = df_results_chart[rank_by].rank(method='first', ascending=rank_asc)
+    df_results_chart = df_results_chart.sort_values('rank')
+
+    time_got_results = time.time()
+    print(f'... time ks to results = {round(time_got_results - time_got_ks, 2)}')
+
+    time_done = time.time()
+    print(f'... time total = {round(time_done - time_start, 2)}')
 
     # build response
     if response_format == 'html':
@@ -91,5 +100,9 @@ def results():
             } for result in results
         ]
         return render_template('results.html', charts=charts)
-    else:
+    elif response_format == 'json':
         return jsonify(results)
+    elif response_format == 'csv':
+        df_results_chart.to_string()
+    else:
+        return None
