9df1de36ffcfad1499557587f5c5c723cf46633f
Andrew Maguire <andrewm4894@gmail.com>
2020-06-05 16:48:22 +0100

    dev

--- get_data.py
+++ get_data.py
@@ -7,6 +7,7 @@ from utils import filter_useless_cols
 
 async def get_chart_df_async(api_call, data):
     url, chart = api_call
+    print(url)
     r = await asks.get(url)
     r_json = r.json()
     df = pd.DataFrame(r_json['data'], columns=['time_idx'] + r_json['labels'][1:])
