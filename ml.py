4932b1b0f992176b11cb6826cb5d7b4aca925aba
Andrew Maguire <andrewm4894@gmail.com>
2020-06-08 15:09:49 +0100

    dev

--- ml.py
+++ ml.py
@@ -1,11 +1,11 @@
 import numpy as np
-from pyod.models.knn import KNN
+from pyod.models.hbos import HBOS as PyODModel
 
 
-def do_knn(chart_cols, arr_baseline, arr_highlight):
+def do_pyod(chart_cols, arr_baseline, arr_highlight):
     results = []
     for chart in chart_cols:
-        model = KNN(contamination=0.01, n_neighbors=5)
+        model = PyODModel(contamination=0.01)
         model.fit(arr_baseline[:, chart_cols[chart]])
         anomaly_preds = model.predict(arr_highlight[:, chart_cols[chart]])
         anomaly_probs = model.predict_proba(arr_highlight[:, chart_cols[chart]])[:, 1]
