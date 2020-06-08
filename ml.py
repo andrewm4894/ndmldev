18cca1028dfa62cb673d8163b9b80ac1dae1bded
Andrew Maguire <andrewm4894@gmail.com>
2020-06-08 21:44:45 +0100

    dev

--- ml.py
+++ ml.py
@@ -20,8 +20,8 @@ def do_pyod(chart_cols, arr_baseline, arr_highlight, n_lags=2):
         arr_baseline = add_lags(arr_baseline, n_lags=n_lags)
         arr_highlight = add_lags(arr_highlight, n_lags=n_lags)
     results = []
+    model = PyODModel(contamination=0.1)
     for chart in chart_cols:
-        model = PyODModel(contamination=0.1)
         model.fit(arr_baseline[:, chart_cols[chart]])
         preds = model.predict(arr_highlight[:, chart_cols[chart]])
         probs = model.predict_proba(arr_highlight[:, chart_cols[chart]])[:, 1]
