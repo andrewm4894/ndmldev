eeaaf981e615e08382d41b22131ac2e445cec459
Andrew Maguire <andrewm4894@gmail.com>
2020-06-10 13:14:45 +0100

    dev

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ async def get_chart_df_async(api_call, data):
     r = await asks.get(url)
     r_json = r.json()
     df = pd.DataFrame(r_json['data'], columns=['time_idx'] + r_json['labels'][1:])
-    df = df.set_index('time_idx').add_prefix(f'{chart}__')
+    df = df.set_index('time_idx').add_prefix(f'{chart}|')
     data[chart] = df
 
 
--- model.py
+++ model.py
@@ -39,7 +39,7 @@ def do_ks(colnames, arr_baseline, arr_highlight):
         ks_stat, p_value = ks_2samp(arr_baseline[:, n], arr_highlight[:, n], mode='asymp')
         results.append([ks_stat, p_value])
     # wrangle results
-    results = zip([[col.split('__')[0], col.split('__')[1]] for col in colnames], results)
+    results = zip([[col.split('|')[0], col.split('|')[1]] for col in colnames], results)
     # ('chart', 'dimension', 'ks', 'p')
     results = [[x[0][0], x[0][1], x[1][0], x[1][1]] for x in results]
     return results
