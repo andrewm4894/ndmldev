9d60d4f9fa8e2f56415228871f42fecf203470e2
Andrew Maguire <andrewm4894@gmail.com>
2020-06-15 13:59:49 +0100

    dev

--- utils.py
+++ utils.py
@@ -141,6 +141,7 @@ def results_to_df(results, model):
         df_results_chart.columns = ['_'.join(col) for col in df_results_chart.columns]
         df_results_chart = df_results_chart.reset_index()
         df_results_chart['rank'] = df_results_chart[rank_by].rank(method='first', ascending=rank_asc)
+        df_results_chart = df_results_chart.rename(columns={"score_mean": "score"})
         df_results_chart = df_results_chart.sort_values('rank')
 
     else:
