5b7d2762f6feb05ef1da2902d66238e93a78b2a1
Andrew Maguire <andrewm4894@gmail.com>
2020-06-22 15:19:46 +0100

    dev

--- utils.py
+++ utils.py
@@ -121,3 +121,43 @@ def results_to_df(results_dict, score_thold):
 
     return df_results
 
+
+def get_col_map(colnames, model, model_level):
+    col_map = {}
+    if model_level == 'chart' and model in chart_level_models:
+        charts_list = list(set([colname.split('|')[0] for colname in colnames]))
+        for chart in charts_list:
+            col_map[chart] = [colnames.index(colname) for colname in colnames if colname.startswith(f'{chart}|')]
+    else:
+        for col in colnames:
+            col_map[col] = [colnames.index(colname) for colname in colnames if colname == col]
+    return col_map
+
+
+def summary_info(n_charts, n_dims, n_bad_data, fit_success, fit_fail, fit_default, model_level):
+    # log some summary stats
+    if model_level == 'chart':
+        success_rate = round(fit_success / n_charts, 2)
+        bad_data_rate = round(n_bad_data / n_charts, 2)
+    else:
+        bad_data_rate = round(n_bad_data / n_dims, 2)
+        success_rate = round(fit_success / n_dims, 2)
+    msg = f"... success_rate={success_rate}, bad_data_rate={bad_data_rate}, charts={n_charts}, dims={n_dims}"
+    msg += f", bad_data={n_bad_data}, fit_success={fit_success}, fit_fail={fit_fail}, fit_default={fit_default}'"
+    return msg
+
+
+def add_lags(data, n_lags=1, data_type='np'):
+    data_orig = np.copy(data)
+    if data_type == 'np':
+        for n_lag in range(1, n_lags + 1):
+            data = np.concatenate((data, np.roll(data_orig, n_lag, axis=0)), axis=1)
+        data = data[n_lags:]
+    elif data_type == 'df':
+        colnames_new = [f"{col}_lag{n_lag}".replace('_lag0', '') for n_lag in range(n_lags+1) for col in data.columns]
+        data = pd.concat([data.shift(n_lag) for n_lag in range(n_lags + 1)], axis=1)
+        data.columns = colnames_new
+    log.debug(f'... (add_lags) n_lags = {n_lags} arr_orig.shape = {data_orig.shape}  arr.shape = {data.shape}')
+    return data
+
+
