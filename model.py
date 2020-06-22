fdff9c20f1031d522867b888e6c9544df8e9214d
Andrew Maguire <andrewm4894@gmail.com>
2020-06-22 15:06:54 +0100

    dev

--- model.py
+++ model.py
@@ -359,13 +359,9 @@ def add_lags(data, n_lags=1, data_type='np'):
             data = np.concatenate((data, np.roll(data_orig, n_lag, axis=0)), axis=1)
         data = data[n_lags:]
     elif data_type == 'df':
-        print(data.shape)
-        print(data.head())
+        colnames_new = [f"{col}_lag{n_lag}".replace('_lag0', '') for n_lag in range(n_lags) for col in data.columns]
         data = pd.concat([data.shift(n_lag) for n_lag in range(n_lags + 1)], axis=1)
-        print(data.shape)
-        print(data.head())
-        print([f"{col}_lag{n_lag}".replace('_lag0', '') for n_lag in range(n_lags) for col in data.columns])
-        data.columns = [f"{col}_lag{n_lag}".replace('_lag0', '') for n_lag in range(n_lags) for col in data.columns]
+        data.columns = colnames_new
     log.debug(f'... (add_lags) n_lags = {n_lags} arr_orig.shape = {data_orig.shape}  arr.shape = {data.shape}')
     return data
 
