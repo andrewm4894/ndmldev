8181fcbabe208a0ad23da5f580d20d6de1ec0538
Andrew Maguire <andrewm4894@gmail.com>
2020-06-22 15:11:58 +0100

    dev

--- model.py
+++ model.py
@@ -347,8 +347,8 @@ def summary_info(n_charts, n_dims, n_bad_data, fit_success, fit_fail, fit_defaul
     else:
         bad_data_rate = round(n_bad_data / n_dims, 2)
         success_rate = round(fit_success / n_dims, 2)
-    msg = f"... success_rate={success_rate}, bad_data_rate={bad_data_rate}, dims={n_dims}, bad_data={n_bad_data}"
-    msg += f", fit_success={fit_success}, fit_fail={fit_fail}, fit_default={fit_default}'"
+    msg = f"... success_rate={success_rate}, bad_data_rate={bad_data_rate}, charts={n_charts}, dims={n_dims}"
+    msg += f", bad_data={n_bad_data}, fit_success={fit_success}, fit_fail={fit_fail}, fit_default={fit_default}'"
     return msg
 
 
